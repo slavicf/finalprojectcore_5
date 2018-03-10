fb4e382850dc26db53735a92870789c18ef228ce
slavicf <jaroslavfrunt@gmail.com>
2018-03-10 14:39:01 +0200

    Реорганизовал проект

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,50 @@
 import com.mashape.unirest.http.exceptions.UnirestException;
-import queries.Tasks;
+import queries.t1showglobalinfochannel.ShowGlobalInfoChannel;
+import queries.t3sortchannelsbydata.SortChannelsByData;
 
 public class Main {
+
+    private static void task1() throws UnirestException {
+        System.out.println("task1:");
+        ShowGlobalInfoChannel showGlobalInfoChannel = new ShowGlobalInfoChannel("UCuXYmUOJSbEH1x88WUV1aMg");
+        System.out.println("Имя канала " + showGlobalInfoChannel.getTitle() +
+                "\nДата создания канала " + showGlobalInfoChannel.getPublishedAt() +
+                "\nКол-во подписчиков " + showGlobalInfoChannel.getSubscriberCount() +
+                "\nКол-во видео на канале " + showGlobalInfoChannel.getVideoCount() +
+                "\nКол-во просмотров всех видео " + showGlobalInfoChannel.getViewCount());
+    }
+
+    private static void task2() {
+
+    }
+
+    private static void task3() throws UnirestException {
+        System.out.println("task2:");
+        String[] channelIds = {"UCmBkHKLRjfHDfFyhrZhowcQ",
+                "UCuXYmUOJSbEH1x88WUV1aMg",
+                "UCuvrjFN3uRkBT3_VUlhKqcA",
+                "UCC7qpnId5RIQruKDJOt2exw",
+                "UCoICDijraUJjIZNrPy-mRDQ"};
+        SortChannelsByData sortChannelsByData = new SortChannelsByData(channelIds);
+        sortChannelsByData.sortViewCount();
+    }
+
+    private static void task4() {
+
+    }
+
+    private static void task5() {
+
+    }
+
     public static void main(String[] args) throws UnirestException {
         System.out.println("final project core");
 
-        Tasks.task1();
-        Tasks.task2();
-        Tasks.task3();
-        Tasks.task4();
-        Tasks.task5();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
 
         System.out.println("task3:");
 
